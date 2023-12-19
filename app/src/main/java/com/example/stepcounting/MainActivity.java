8cad8f56870740b73e57b4b1ed25700369b1a8ac
stunningMary <lyublyukotikov.m.y.r@mail.ru>
2023-12-19 10:57:56 +0500

    first commit

---
+++ app/src/main/java/com/example/stepcounting/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.stepcounting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    public boolean active = true;
+    private SensorManager sensorManager;
+    private int count = 0;
+    private TextView text; //Ссылка на ТextView
+    private long lastUpdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView2);
+        text.setText(String.valueOf(count));
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdate = System.currentTimeMillis();
+    }
+    @Override
+    protected void onResume(){ // продолжить
+        super.onResume(); // подписываемся на действие
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL); // запускаем датчик
+    }
+    @Override
+    protected void onPause(){ // nayзa
+        super.onPause(); // подписываемся на действие
+        sensorManager.unregisterListener((SensorEventListener) this); // останавливаем датчик
+    }
+    public void onStoped(View view) {
+        active = !active;
+        if (!active) {
+            Button button = findViewById(R.id.button); // находим кнопку
+            button.setText("ВОЗОБНОВИТЬ"); // присваиваем текст
+        } else { // если активно
+            Button button = findViewById(R.id.button); // находим кнопку
+            button.setText("ПАУЗА");
+        }
+    }
+    public void onSensorChanged(SensorEvent event){
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            float[] values= event.values;
+            float x= values[0];
+            float y= values[1];
+            float z= values[2];
+
+            float accelationSquareRoot=(x* x+ y* y+ z* z)
+                    /(SensorManager.GRAVITY_EARTH* SensorManager.GRAVITY_EARTH);
+
+            //Текущее время
+            long actualTime=System.currentTimeMillis();
+            if(accelationSquareRoot>=2) //Если тряска сильная
+            {
+                if(actualTime- lastUpdate<200){
+                    return;
+                }
+                lastUpdate= actualTime; // актуализируем данные
+                count ++;
+                text.setText(String.valueOf(count)); // обновляем текст
+            }
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
